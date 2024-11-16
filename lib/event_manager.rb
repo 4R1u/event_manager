d5349bf62ef732a80e1f0b8bd951e1a2f65c4a94
4R1u <62248461+4R1u@users.noreply.github.com>
2024-11-16 17:26:49 +0400

    Add Basic Algorithm Plan in Comments

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -12,5 +12,10 @@ contents =  CSV.open(
 contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
+
+  # If the zipcode is exactly five digits, then assume it is ok.
+  # If the zipcode is more than five digits, then truncate it to the first five digits.
+  # If the zipcode is less than five digits, then add zeros to the front until it becomes five digits.
+
   puts "#{name} #{zipcode}"
 end
