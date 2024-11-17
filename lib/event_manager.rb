f08e54008797d046fb93077957638f8152ba164a
4R1u <62248461+4R1u@users.noreply.github.com>
2024-11-17 12:48:51 +0400

    Add Basic Loop to Find Legislators

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 require 'csv'
-puts 'Event Manager Initialized!'
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
+
+puts 'EventManager Initialized!'
 
 contents = CSV.open(
   'event_attendees.csv',
@@ -15,7 +20,16 @@ contents = CSV.open(
 
 contents.each do |row|
   name = row[:first_name]
+
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  legislators = civic_info.representative_info_by_address(
+    address: zipcode,
+    levels: 'country',
+    roles: %w[legislatorUpperBody legislatorLowerBody]
+  )
+
+  legislators = legislators.officials
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
