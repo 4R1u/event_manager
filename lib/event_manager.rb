a7e704fdd96cf83d4bc7b93b26f11e20613bbf5c
4R1u <62248461+4R1u@users.noreply.github.com>
2024-11-17 09:08:40 +0400

    Move Cleaning Zipcodes into Its Own Function

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,7 +3,19 @@
 require 'csv'
 puts 'Event Manager Initialized!'
 
-contents =  CSV.open(
+def clean_zipcode(zipcode)
+  if zipcode.nil?
+    '00000'
+  elsif zipcode.length < 5
+    zipcode.rjust(5, '0')
+  elsif zipcode.length > 5
+    zipcode[0..4]
+  else
+    zipcode
+  end
+end
+
+contents = CSV.open(
   'event_attendees.csv',
   headers: true,
   header_converters: :symbol
@@ -12,14 +24,6 @@ contents =  CSV.open(
 contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
-
-  if zipcode.nil?
-    zipcode = '00000'
-  elsif zipcode.length < 5
-    zipcode = zipcode.rjust(5, '0')
-  elsif zipcode.length > 5
-    zipcode = zipcode[0..4]
-  end
 
   puts "#{name} #{zipcode}"
 end
